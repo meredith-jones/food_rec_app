8eddb4cea07eb4fe63009b033da36a84f1fcc6dc
Derek Koh <dkoh@linkedin.com>
2016-06-19 09:04:36 -0700

    Got the google call to work

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,20 @@ var https = require("https");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'What are we going to do now' });
 });
 
 router.get('/quiz', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../views/quiz.html'));
 });
 
+router.get('/dog/', function(req, res) {
+   
+});
 
+router.get('/testme/', function(req, res, next) {
 
-router.get('/testme', function(req, res, next) {
-
-	var queryparam = "Starbucks"
-	https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.598748,-122.388726&radius=500&type=restaurant&key=AIzaSyBURdJOkG62jXYReG_GIyHwOvWo9YIxG28
-	var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.598748,-122.388726&radius=1000&keyword="+queryparam+"&key=AIzaSyBURdJOkG62jXYReG_GIyHwOvWo9YIxG28";
+	var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.598748,-122.388726&radius=1000&keyword="+req.query.queryparam+"&key=AIzaSyBURdJOkG62jXYReG_GIyHwOvWo9YIxG28";
 	https.get(url, function (response) {
 	    // data is streamed in chunks from the server
 	    // so we have to handle the "data" event    
@@ -36,12 +36,12 @@ router.get('/testme', function(req, res, next) {
 	        //console.log(buffer);
 	        console.log("got buffer \n");
 	        data = JSON.parse(buffer);
-	       // route = data.routes[0];
+	       
 
 	        // extract the distance and time
 	       // console.log("Walking Distance: " + route.legs[0].distance.text);
 	       // console.log("Time: " + route.legs[0].duration.text);
-        	res.send(data)
+        	res.send(buffer);
 	    }); 
 	}); 
 
